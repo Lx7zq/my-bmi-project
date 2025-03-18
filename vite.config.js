d46c70222e794d5c42f64716052ab8e9ca8fe6b1
Lx7zq <thisforworkonly7@gmail.com>
2025-03-18 14:44:48 +0700

    Add Docker

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/my-bmi-project",
+  base:"my-bmi-project",
   plugins: [react()],
 });
